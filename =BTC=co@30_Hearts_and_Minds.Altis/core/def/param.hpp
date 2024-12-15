2bd691e8e4fadfc7de5f3f3006aed7b787732710
TomDraal <122184262+TomDraal@users.noreply.github.com>
2024-12-16 00:28:51 +0100

    Update =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
    Co-authored-by: Vdauphin <vdauphin@outlook.fr>

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -407,7 +407,7 @@ class Params {
         texts[] = {$STR_DISABLED,$STR_ENABLED};
         default = 1;
     };
-    class btc_p_chem_cache_probability { // Advanced Chemical warfare setting to change probability of a chemical weapon cache
+    class btc_p_chem_cache_probability { // Chemical weapon cache probability:
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_GAMEPLAY_CHEM_CACHE_PROBABILITY"]);
         values[]={0,10,20,30,40,50,60,70,80,90,100};
         texts[]={"0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
